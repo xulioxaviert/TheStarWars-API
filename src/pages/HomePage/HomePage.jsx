a019406ebd02a1f3d430121f5a6644b97fef7d46
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-04-02 19:25:41 +0200

    chore: finished project

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
-import React, { useEffect, useState } from "react";
-import "./_homepage.scss";
+import { React } from "react";
+import '../../main.scss';
+//import { Component } from "react";
+import ReactPlayer from "react-player";
 
 export const HomePage = () => {
-  
-  return <h1>Home Page</h1>
+  return (
+    <div className="container-video">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=zMeQmrzFrAw"
+        className="react-player"
+        playing
+        width="1000px"
+        height="700px"
+        volume="0.3"
+      />
+    </div>
+  );
 };
